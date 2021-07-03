60e30c9fe716a706f2de276a86c955399a5eef73
Ulukbek Attokurov <uattokurov@THB-ARG-161.local>
2021-07-03 15:51:46 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return "Hello world"
+
+@app.get("/about")
+def about():
+    return {'data': {'about page': 'salam'}}
+
